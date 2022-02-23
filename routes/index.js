092df09b287f9e59b838e3755c2a4ef495e69e04
Roby Romadany <robyromadany.klink@gmail.com>
2022-02-23 18:03:52 +0700

    tes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', first_name: 'Roby', tes: process.env.first_name });
+  res.render('index', { title: 'Express', last_name: ' Romadany', tes: process.env.first_name });
 });
 
 module.exports = router;
